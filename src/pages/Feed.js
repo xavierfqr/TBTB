c9f494512b8566c904be2179fada471a1697a380
Xavier <facqueur.xavier@gmail.com>
2021-08-25 17:05:00 +0200

    Add : Feed fetch posts

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,11 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { SignOutButton } from './Home';
 import { UserContext } from '../lib/context';
+import { storage, firestore } from '../lib/firebase';
+import Posts from '../components/Posts';
+
+
+const LIMIT = 10;
+
 
 
 function Feed() {
     const {user, username} = useContext(UserContext);
+    const [posts, setPosts] = useState([]);
+
+    async function fetchData() {
+        const postsQuery = await firestore.collectionGroup('posts').limit(LIMIT);
+        await postsQuery.get().then(querySnapshot => {
+            const postList = [];
+            querySnapshot.forEach(post => {
+                postList.push(post.data())
+            })
+            setPosts(postList);
+        });
+        // const postList = postsGet.docs.map(doc => doc.data());
+        // setPosts(postList)
+        // console.log(posts)
+    } 
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         user && <div>
@@ -14,6 +39,7 @@ function Feed() {
             <Link to={`profile/${user.displayName}`}>
                 <img src={user.photoURL} alt="profile"/>
             </Link>
+            <Posts posts={posts}/>
         </div>
     )
 }
